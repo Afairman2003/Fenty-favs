7dd5d96f483fc23e6643f05c7310370e95afa93e
Afairman2003 <afairman@brooklynsteamcenter.org>
2023-05-08 04:32:54 -0400

    delete post

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -12,14 +12,30 @@ const main = async () => {
   
   const postForm = document.querySelector('#post-form');
   const postContainer = document.querySelector('#post-container');
+  
 
   const displayPost = async (post) => {
     const { url, caption } = post;
+    const divElement = document.createElement('div');
     const imgElement = document.createElement('img');
     const captionElement = document.createElement('p');
+    const buttonElement = document.createElement('button');
+    divElement.setAttribute('data-post-id', post.id);
+    buttonElement.innerText = 'Delete';
+    buttonElement.id = "delete-post-button";
     imgElement.src = url;
     captionElement.innerText = caption;
-    postContainer.append(imgElement, captionElement);
+    divElement.append(imgElement, captionElement, buttonElement);
+    postContainer.append(divElement);
+
+    buttonElement.addEventListener('click', async (e) => {
+      e.preventDefault();
+      const id = e.target.parentElement.attributes[0].value
+      const [posts, err] = await handleFetch(`/api/posts/${id}`,{ method: 'DELETE' });
+      console.log(posts);
+      displayPost(posts.id);
+    })
+  
   }
 
   const [secret, _err] = await handleFetch('/api/logged-in-secret');
@@ -28,7 +44,13 @@ const main = async () => {
     document.querySelector('#secret-message').textContent = secret.msg;
   }
 
-  
+  async function getPosts() {
+    const [response, err] = await handleFetch('/api/posts');
+    console.log('response, err:', response, err);
+    response.forEach(post => displayPost(post.id)); // for each
+  }
+  getPosts();
+
   postForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const url = document.querySelector('#image').value;
@@ -36,8 +58,9 @@ const main = async () => {
     const [posts, err] = await handleFetch('/api/posts', getFetchOptions({ url, caption }));
     console.log(posts);
     displayPost(posts.id);
-  })
 
+  })
+  
 };
 
 main();
